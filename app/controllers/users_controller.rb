0390616dd9836683a9d4808d8dd0148d8a81dbf4
AlabushevEvgeniy <alabushev-evgeniy@yandex.ru>
2019-03-24 13:45:50 +0500

    added authorization on creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ before_action :authorize_user, except: [:index, :new, :create, :show]
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно создан!'
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: 'Пользователь успешно создан и авторизован!'
     else
       render 'new'
     end
